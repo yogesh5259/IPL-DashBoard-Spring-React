21da5c567aef37120e98dbaa4a34babc41d880ec
MacBook <patelyogesh5040@gmail.com>
2021-04-25 00:13:02 +0530

    Spring + React

---
+++ src/main/java/com/springboot/project/ipldashbord/IpldashbordApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.project.ipldashbord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpldashbordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpldashbordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/project/ipldashbord/IpldashbordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.project.ipldashbord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpldashbordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
