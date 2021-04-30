229b14aa848ef58baf225ba50831083df86bb049
MacBook <patelyogesh5040@gmail.com>
2021-05-01 01:51:51 +0530

    Team API

--- src/main/java/com/springboot/project/ipldashbord/controller/TeamController.java
+++ src/main/java/com/springboot/project/ipldashbord/controller/TeamController.java
@@ -12,21 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class TeamController {
 
     private TeamRepository teamRepository;
-
     private MatchRepository matchRepository;
 
-
+    
 
     public TeamController(TeamRepository teamRepository, MatchRepository matchRepository) {
         this.teamRepository = teamRepository;
         this.matchRepository = matchRepository;
     }
 
+
+
     @GetMapping("/team/{teamName}")
     public Team getTeam(@PathVariable String teamName) {
         Team team = this.teamRepository.findByTeamName(teamName);
-        team.setMatches(this.matchRepository.getByTeam1orTeam2(teamName, teamName));
-
+        team.setMatches(this.matchRepository.findLatestMatchesByTeam(teamName,4));
         return team;
     }
 
--- src/main/java/com/springboot/project/ipldashbord/repository/MatchRepository.java
+++ src/main/java/com/springboot/project/ipldashbord/repository/MatchRepository.java
@@ -4,8 +4,15 @@ import java.util.List;
 
 import com.springboot.project.ipldashbord.model.Match;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MatchRepository extends CrudRepository<Match,Long> {
-    List<Match> getByTeam1orTeam2(String teamName1,String teamName2);
+    List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1,String teamName2,Pageable pageable);
+
+    default List<Match> findLatestMatchesByTeam(String teamName, int count){
+        Pageable pageable = PageRequest.of(0, 4);
+        return getByTeam1OrTeam2OrderByDateDesc(teamName,teamName,pageable);
+    }
 }
