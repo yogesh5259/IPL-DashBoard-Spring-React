57c26a6a729d9add692885c6ce3b31220dbb3230
MacBook <patelyogesh5040@gmail.com>
2021-05-23 00:27:51 +0530

    Spring + react

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
-import { TeamPage } from './pages/TeamPage';
+import "./App.css";
+import { TeamPage } from "./pages/TeamPage";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <TeamPage/>
-      
+      <Router>
+        <Route path="/team/:teamName">
+          <TeamPage />
+        </Route>
+      </Router>
     </div>
   );
 }
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,27 +1,31 @@
 import { React, useEffect, useState } from 'react';
+import {useParams} from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const TeamPage = () =>{
   const [team, setTeam] = useState({matches:[]});
- 
+  const {teamName} = useParams(); 
   useEffect(
     ()=>{
       const fetchMatches = async() => {
-        const response = await fetch('http://localhost:8080/team/Sunrisers%20Hyderabad');
+        const response = await fetch(`http://localhost:8080/team/${teamName}`);
         const data = await response.json();
         setTeam(data);
 
       };
       fetchMatches();
-    },[]
+    },[teamName]
   );
 
+  if(!team || !team.teamName){
+    return <h1>Team Not found</h1>
+  }
 
   return (
     <div className="TeamPage">
       <h1>{team.teamName}</h1>
-      <MatchDetailCard match={team.matches[0]}/>
+      <MatchDetailCard teamName = {team.Name} match={team.matches[0]}/>
       {team.matches.slice(1).map(match => <MatchSmallCard match = {match}/>)}
     </div>
   );
